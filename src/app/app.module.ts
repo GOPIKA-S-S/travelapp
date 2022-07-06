ddc6a43069b4182d51beb1726534fb2c382812da
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-07 01:46:34 +0530

    searchplace completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
 import { ViewdestinationComponent } from './viewdestination/viewdestination.component';
+import { SearchplacesComponent } from './searchplaces/searchplaces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TravelRegistrationComponent,
-    ViewdestinationComponent
+    ViewdestinationComponent,
+    SearchplacesComponent
   ],
   imports: [
     BrowserModule,
